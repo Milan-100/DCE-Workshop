e7736eeb9c5dabba05c3df9bbed90f1507af1c83
Milan-100 <157277607+Milan-100@users.noreply.github.com>
2024-02-09 14:16:48 +0530

    Create elseif.sol

---
+++ elseif.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ifelse{
+   function checkoddeven(int num) public pure returns (int) {
+    if (num % 2 == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+    }
